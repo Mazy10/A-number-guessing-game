73bff8be77cd7e8c757cc7d17fb6b7812bbd31b1
Mazy10 <mazharuluddin@gmail.com>
2026-04-01 17:12:07 +0100

    fixing grammar in print scripts

--- a-number-guessing-game-v1/guessing_game.py
+++ a-number-guessing-game-v1/guessing_game.py
@@ -4,27 +4,34 @@ Project 1 - A Number Guessing Game
 --------------------------------
 """
 
-# Import the random and statistics modules.
 import statistics
 import random
 
 def start_game():
-    print("welcome to the guessing game")
+    print("Welcome to the guessing game!")
     guesses_list = []
     play_again = "y"
 
     while play_again == "y":
-        winning_number = random.randint(1,10) 
+
+        if guesses_list:
+            print(f'The highest score is: {min(guesses_list)}')
+        else:
+            print("No high score. Can you be the first?")
+        winning_number = random.randint(1,50) 
         guesses = 0   
         while True:
             try:
                 guess = int(input("What is the correct number? "))
-                guesses += 1            
-                print("Not valid value. Please try again")
+                guesses += 1
+                if guess > 50:
+                    print("Oops! This number is outside the range. Please try again")                  
+                    continue                 
+                
                 if guess > winning_number:
-                    print("it's lower")
+                    print("It's lower")
                 elif guess < winning_number:
-                    print("it's higher")
+                    print("It's higher")
                 else:
                     print("Got it")
                     guesses_list.append(guesses)
@@ -35,42 +42,41 @@ def start_game():
                     print(guesses_list)
                     break
             except ValueError:
-                print("Enter a valid number")
+                print("Please enter a valid number")
         play_again = input("Do you want to play again? (y/n) ")
         if play_again.lower() == "y":
             continue
         else:
-            print("Thanks for playing! See you again soon.")
+            print("Thanks for playing! See you again soon :)")
             break
-
 
 start_game()
 
 
-# Create the start_game function.
-# Write your code inside this function.
+# Create the start_game function. done
+# Write your code inside this function. done
   
    #   When the program starts, we want to:
 #   ------------------------------------
-#   1. Display an intro/welcome message to the player.
+#   1. Display an intro/welcome message to the player. done
 
-#   2. Store a random number as the answer/solution.
-    
-#   3. Continuously prompt the player for a guess.
+#   2. Store a random number as the answer/solution. done
+     
+#   3. Continuously prompt the player for a guess. done
 #     a. If the guess is greater than the solution, display to the player "It's lower".
 #     b. If the guess is less than the solution, display to the player "It's higher".
-#   4. Once the guess is correct, stop looping, inform the user they "Got it" and store the number of guesses it took in a list.
+#   4. Once the guess is correct, stop looping, inform the user they "Got it" and store the number of guesses it took in a list. done
         
-#   5. Display the following data to the player
+#   5. Display the following data to the player done
 #     a. How many attempts it took them to get the correct number in this game
 #     b. The mean of the saved attempts list
 #     c. The median of the saved attempts list
 #     d. The mode of the saved attempts list
-#   6. Prompt the player to play again
+#   6. Prompt the player to play again done
 #     a. If they decide to play again, start the game loop over.
 #     b. If they decide to quit, show them a goodbye message.
 
 # ( You can add more features/enhancements if you'd like to. )
 
 
-# Kick off the program by calling the start_game function.
+# Kick off the program by calling the start_game function. done
