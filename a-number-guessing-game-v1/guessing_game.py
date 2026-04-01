8991f80113c21f26e562066db584e3ed8732cea9
Mazy10 <mazharuluddin@gmail.com>
2026-04-01 13:47:20 +0100

    added the list function for attempts

--- a-number-guessing-game-v1/guessing_game.py
+++ a-number-guessing-game-v1/guessing_game.py
@@ -5,25 +5,42 @@ Project 1 - A Number Guessing Game
 """
 
 # Import the random and statistics modules.
-from statistics import median
-from statistics import mode
-from statistics import mean
+import statistics
 import random
+
+def start_game():
+    print("welcome to the guessing game")
+    guesses_list = []
+    winning_number = random.randit(1,50) 
+    guess = 0   
+    while True:
+        guess = int(input("What is the correct number? "))
+        guess += 1
+        if guess > winning_number:
+            print("it's lower")
+        elif guess < winning_number:
+            print("it's higher")
+        else:
+            print("Got it")
+            break
+
+start_game()
+
 
 # Create the start_game function.
 # Write your code inside this function.
-def start_game():  
+  
    #   When the program starts, we want to:
 #   ------------------------------------
 #   1. Display an intro/welcome message to the player.
-    print("welcome to the guessing game")
+
 #   2. Store a random number as the answer/solution.
     
 #   3. Continuously prompt the player for a guess.
 #     a. If the guess is greater than the solution, display to the player "It's lower".
 #     b. If the guess is less than the solution, display to the player "It's higher".
-
 #   4. Once the guess is correct, stop looping, inform the user they "Got it" and store the number of guesses it took in a list.
+        
 #   5. Display the following data to the player
 #     a. How many attempts it took them to get the correct number in this game
 #     b. The mean of the saved attempts list
