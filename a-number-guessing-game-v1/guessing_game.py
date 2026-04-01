29df8a6c42a796661c41aef30cd6c2b090c06dc0
Mazy10 <mazharuluddin@gmail.com>
2026-04-01 14:38:54 +0100

    adding code to handle errors and exceptions

--- a-number-guessing-game-v1/guessing_game.py
+++ a-number-guessing-game-v1/guessing_game.py
@@ -17,21 +17,25 @@ def start_game():
         winning_number = random.randint(1,10) 
         guesses = 0   
         while True:
-            guess = int(input("What is the correct number? "))
-            guesses += 1
-            if guess > winning_number:
-                print("it's lower")
-            elif guess < winning_number:
-                print("it's higher")
-            else:
-                print("Got it")
-                guesses_list.append(guesses)
-                print(f'The number of guesses it took you was: {guesses}')
-                print(f'The mean of the number of attempts was: {statistics.mean(guesses_list)}')
-                print(f'The median of the number of attempts was: {statistics.median(guesses_list)}')
-                print(f'The mode of the number of attempts was: {statistics.mode(guesses_list)}')
-                print(guesses_list)
-                break
+            try:
+                guess = int(input("What is the correct number? "))
+                guesses += 1            
+                print("Not valid value. Please try again")
+                if guess > winning_number:
+                    print("it's lower")
+                elif guess < winning_number:
+                    print("it's higher")
+                else:
+                    print("Got it")
+                    guesses_list.append(guesses)
+                    print(f'The number of guesses it took you was: {guesses}')
+                    print(f'The mean of the number of attempts was: {statistics.mean(guesses_list)}')
+                    print(f'The median of the number of attempts was: {statistics.median(guesses_list)}')
+                    print(f'The mode of the number of attempts was: {statistics.mode(guesses_list)}')
+                    print(guesses_list)
+                    break
+            except ValueError:
+                print("Enter a valid number")
         play_again = input("Do you want to play again? (y/n) ")
         if play_again.lower() == "y":
             continue
