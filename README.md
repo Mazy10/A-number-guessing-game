0eb469057828d4db81c56647b65157e9f12ac096
Mazy10 <mazharuluddin@gmail.com>
2026-04-02 14:58:24 +0100

    adding description in read me file

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 A NUMBER GUESSING GAME
 
+This is a simple python game where the computer generates a random number between 1 and 50, and the player tries to guess it. After each guess, the game tells the player whether the correct number is higher or lower of their guess. The game tracks how many guesses it takes to win and keeps a high score based on the lowest number of guesses or attempts it took. 
+
+How to Run:
+
+1. Python must be intalled on your computer.
+2. Download the game file.
+3. Open the terminal or command prompt. 
+4. Navigate to the folder where the file is saved. 
+5. Run the program using:
+    python guessing_game.py
+
+How to Play:
+
+1. Enter each number between 1 and 50 when prompted.
+2. The game will tell you if your guess is too high or too low.
+3. Keep guessing until you get the correct number. 
+4. After winning, you will see how many guesses it took. 
+5. Try to beat your high score which is the lowest number of guesses. 
+6. Choose whether to play again or quit. 
--- a-number-guessing-game-v1/guessing_game.py
+++ a-number-guessing-game-v1/guessing_game.py
@@ -52,31 +52,3 @@ def start_game():
 
 start_game()
 
-
-# Create the start_game function. done
-# Write your code inside this function. done
-  
-   #   When the program starts, we want to:
-#   ------------------------------------
-#   1. Display an intro/welcome message to the player. done
-
-#   2. Store a random number as the answer/solution. done
-     
-#   3. Continuously prompt the player for a guess. done
-#     a. If the guess is greater than the solution, display to the player "It's lower".
-#     b. If the guess is less than the solution, display to the player "It's higher".
-#   4. Once the guess is correct, stop looping, inform the user they "Got it" and store the number of guesses it took in a list. done
-        
-#   5. Display the following data to the player done
-#     a. How many attempts it took them to get the correct number in this game
-#     b. The mean of the saved attempts list
-#     c. The median of the saved attempts list
-#     d. The mode of the saved attempts list
-#   6. Prompt the player to play again done
-#     a. If they decide to play again, start the game loop over.
-#     b. If they decide to quit, show them a goodbye message.
-
-# ( You can add more features/enhancements if you'd like to. )
-
-
-# Kick off the program by calling the start_game function. done
